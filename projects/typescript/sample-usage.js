d89d57ab5ced03d30e63287e747d9fc520034623
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2019-01-06 19:00:26 +0200

    typescript

---
+++ projects/typescript/sample-usage.js
@@ -0,0 +1,12 @@
+import __ from '../../dist/jb-react-all';
+
+declare var jb: jbObj;
+
+const label : labelPT = {$: 'label',  title: 'xx', features: [], style: {$:'label.h1'}};
+const ctrl : controlType = {$:'image', style:{$:'image.default'}, features:{$: ''}  }
+
+jb.component('xxx', {id: 'xx', type: 'control', impl: {$:'tree', features: [
+    {$: ''}
+]}})
+
+
