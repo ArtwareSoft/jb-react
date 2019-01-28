73747cb86fb7137347bc6c2099926c37ef307f75
Shai Ben-Yehuda <shaiby@wix.com>
2019-01-28 17:17:29 +0200

    studio-devtools

---
+++ hosts/studio-devtools/devtools.js
@@ -0,0 +1,7 @@
+chrome.devtools.panels.create("jBart",
+    "MyPanelIcon.png",
+    "studio/studio.html",
+    function(panel) {
+      // code invoked on panel creation
+    }
+);
---
+++ hosts/studio-devtools/studio/studio-runner.js
@@ -0,0 +1,2 @@
+
+jb.ui.render(jb.ui.h(new jb.jbCtx().run({$:'studio.all'}).reactComp()),document.getElementById('studio'))
