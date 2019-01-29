374c5d9f104990f04b96c5c54155952baa58ddb8
Shai Ben-Yehuda <shaiby@wix.com>
2019-01-29 18:24:12 +0200

    object-encoder-tests

--- projects/ui-tests/object-encoder-tests.js
+++ projects/ui-tests/object-encoder-tests.js
@@ -6,7 +6,7 @@ jb.component('data-test.object-encoder.stringify', {
             return jb.objectEncoder.stringify(top)
         },
 		expectedResult : ctx => { 
-            return JSON.stringify(ctx.data) === `[{"text":1,"leaf1":2,"leaf2":2},"textVal",{"base":3},true]`
+            return ctx.data === `[{"text":1,"leaf1":2,"leaf2":2},"textVal",{"base":3},true]`
         }
 	},
 })
@@ -19,7 +19,22 @@ jb.component('data-test.object-encoder.encode-decode', {
            return {top, result: jb.objectEncoder.decode(jb.objectEncoder.encode(top))}
        },
        expectedResult : ctx => { 
-           return JSON.stringify(ctx.data) === JSON.stringify(ctx.data.result)
+           return JSON.stringify(ctx.data.top) === JSON.stringify(ctx.data.result)
+       }
+   },
+})
+
+jb.component('data-test.object-encoder.encode-jb time', {
+    impl :{$: 'data-test',
+       calculate: ctx =>  {
+           const time = Date.now();
+           const jb2 = jb.objectEncoder.decode(jb.objectEncoder.encode(jb));
+            jb.objectEncoder.decode(jb.objectEncoder.encode(ctx));
+            jb2.comps;
+            return Date.now() - time
+       },
+       expectedResult : ctx => { 
+           return ctx.data < 200 // less than 200 msec
        }
    },
 })
