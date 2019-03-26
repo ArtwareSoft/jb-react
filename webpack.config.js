b3e1beac42cf29242b924bf1e8b077c2baf27f03
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2019-03-26 21:08:16 +0200

    dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,8 @@
 var webpack = require('webpack');
-clearvar path = require('path');
-// var fs = require('fs');
-// require('./src/loader/jb-loader.js');
+var path = require('path');
 
 var JBART_DIR = __dirname + '/';
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
-// function concatFiles(files,target) {
-//   try {
-//   fs.unlinkSync(JBART_DIR +target);
-//   } catch (e) {}
-//   files.map(x=>JBART_DIR +x).forEach(f=>
-//     fs.appendFileSync(JBART_DIR +target,fs.readFileSync(f) + ';\n\n'));
-// }
-
-// var jbReactFiles = [].concat.apply([],[resources['common'],resources['ui-common'],resources['ui-tree'],'dist/export_jb.js']).filter(x=>!x.match(/.css$/));
-// var studioFiles = [].concat.apply([],[resources['common'],resources['ui-common'],resources['ui-tree'],resources['codemirror']]).filter(x=>!x.match(/.css$/))
-//     .concat(resources.studio.map(x=>'projects/studio/studio-' + x + '.js'));
-// var nodeFiles = [].concat.apply([],[resources['common'],resources['node'],resources['pretty-print'],resources['xml'],resources['jison'],resources['parsing']]).filter(x=>!x.match(/.css$/));
-// var coreFiles = resources['core'];
-
-// console.log(jbReactFiles);
-
-// concatFiles(studioFiles,'dist/studio-all.js');
-// concatFiles(jbReactFiles,'dist/jb-react-all.js');
-// concatFiles(nodeFiles,'dist/jb4node.js');
-// concatFiles(coreFiles,'dist/jbart-core.js');
 
 var jbRx = {
   mode: 'development',
