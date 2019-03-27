656954a43bf401883d30dd125cc9bfe21bfed91a
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2019-03-27 08:40:26 +0200

    dist

--- bin/build.phase2.js
+++ bin/build.phase2.js
@@ -5,11 +5,12 @@ require('../src/loader/jb-loader.js');
 var JBART_DIR = __dirname + '/../';
 
 function concatFiles(files,target) {
+  const fn = JBART_DIR + 'dist/' + target;
   try {
-  fs.unlinkSync(JBART_DIR +target);
+    fs.unlinkSync(fn);
   } catch (e) {}
   files.map(x=>JBART_DIR +x).forEach(f=>
-    fs.appendFileSync(JBART_DIR +target,fs.readFileSync(f) + ';\n\n'));
+    fs.appendFileSync(fn,fs.readFileSync(f) + ';\n\n'));
 }
 
 var jbReactFiles = [].concat.apply([],[resources['common'],resources['ui-common'],resources['ui-tree']]).filter(x=>!x.match(/.css$/));
@@ -18,7 +19,8 @@ var studioFiles = [].concat.apply([],[resources['common'],resources['ui-common']
 var nodeFiles = [].concat.apply([],[resources['common'],resources['node'],resources['pretty-print'],resources['xml'],resources['jison'],resources['parsing']]).filter(x=>!x.match(/.css$/));
 var coreFiles = resources['core'];
 
-concatFiles(studioFiles,'dist/studio-all.js');
-concatFiles(jbReactFiles,'dist/jb-react-all.js');
-concatFiles(nodeFiles,'dist/jb4node.js');
-concatFiles(coreFiles,'dist/jbart-core.js');
+concatFiles(studioFiles,'studio-all.js');
+concatFiles(jbReactFiles,'jb-react-all.js');
+concatFiles(nodeFiles,'jb4node.js');
+concatFiles(coreFiles,'jbart-core.js');
+concatFiles(resources['pretty-print'],'pretty-print.js');
