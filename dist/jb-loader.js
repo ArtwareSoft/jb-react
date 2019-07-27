3e9197d9cb8a6fac757274601865a28300b49c03
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2019-07-27 10:23:36 +0300

    jb_loader

---
+++ dist/jb-loader.js
@@ -0,0 +1,172 @@
+var resources = Object.assign((typeof resources != 'undefined' ? resources : {}), {
+      'core': [
+        'src/core/jb-core.js',
+        'src/misc/wSpy.js'
+      ],
+      'common': [
+        'src/core/jb-core.js',
+        'src/core/jb-common.js',
+        'src/misc/wSpy.js',
+      ],
+      'ui-common': [
+//        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/material-design-lite/material.js',
+        'node_modules/material-design-lite/material.css',
+        'node_modules/material-design-lite/dist/material.indigo-pink.min.css',
+
+        'css/font.css',
+        'css/styles.css',
+
+        'dist/jb-preact.js',
+        'dist/jb-immutable.js', // the immutable-helper lib
+        'dist/jb-rx.js',
+
+        'src/ui/react-ctrl.js',
+        'src/ui/immutable-ref.js',
+
+        'src/ui/group.js',
+        'src/ui/label.js',
+        'src/ui/image.js',
+        'src/ui/button.js',
+        'src/ui/field.js',
+        'src/ui/editable-text.js',
+        'src/ui/editable-boolean.js',
+        'src/ui/editable-number.js',
+        'src/ui/common-features.js',
+        'src/ui/css-features.js',
+        'src/ui/dialog.js',
+        'src/ui/menu.js',
+        'src/ui/itemlist.js',
+        'src/ui/itemlist-container.js',
+//        'src/ui/itemlist-with-groups.js',
+        'src/ui/picklist.js',
+        'src/ui/theme.js',
+        'src/ui/icon.js',
+        'src/ui/slider.js',
+        'src/ui/table.js',
+        'src/ui/tabs.js',
+        'src/ui/window.js',
+
+        'src/ui/styles/mdl-styles.js',
+        'src/ui/styles/button-styles.js',
+        'src/ui/styles/editable-text-styles.js',
+        'src/ui/styles/layout-styles.js',
+        'src/ui/styles/group-styles.js',
+        'src/ui/styles/table-styles.js',
+        'src/ui/styles/picklist-styles.js',
+        'src/ui/styles/property-sheet-styles.js',
+        'src/ui/styles/editable-boolean-styles.js',
+				'src/ui/styles/card-styles.js',
+      ],
+      'ui-tree':[
+        'src/ui/tree/tree.js',
+        'src/ui/tree/json-tree-model.js',
+      ],
+      'inner-html': [ // unsafe
+        'src/ui/inner-html.js',
+      ],
+      'testers': [
+        'src/testing/testers.js',
+      ],
+      'codemirror': [
+        'src/ui/styles/codemirror-styles.js',
+        'node_modules/codemirror/lib/codemirror.js',
+        'node_modules/codemirror/mode/xml/xml.js',
+        'node_modules/codemirror/mode/javascript/javascript.js',
+        'node_modules/codemirror/mode/css/css.js',
+        'node_modules/codemirror/mode/jsx/jsx.js',
+        'node_modules/codemirror/mode/htmlmixed/htmlmixed.js',
+        'node_modules/codemirror/addon/hint/show-hint.js',
+//        'node_modules/codemirror/addon/hint/javascript-hint.js',
+        'node_modules/codemirror/addon/hint/xml-hint.js',
+        'node_modules/codemirror/addon/hint/html-hint.js',
+        'node_modules/codemirror/addon/fold/foldgutter.js',
+        'node_modules/codemirror/addon/selection/active-line.js',
+
+        'node_modules/codemirror/lib/codemirror.css',
+        'node_modules/codemirror/theme/solarized.css',
+        'node_modules/codemirror/addon/hint/show-hint.css',
+      ],
+      'd3': [
+        'node_modules/d3/build/d3.js',
+        'src/ui/d3-chart/d3-math.js',
+        'src/ui/d3-chart/d3-chart.js',
+        'src/ui/d3-chart/d3-histogram.js',
+      ],
+      'history': [
+        'node_modules/history/umd/history.js',
+//        'src/ui/url.js'
+      ],
+      'dragula': [
+          'node_modules/dragula/dist/dragula.js',
+          'node_modules/dragula/dist/dragula.css',
+      ],
+      studio: [
+        'utils','path','main','url', 'completion',
+        'toolbar','styles', 'search', 'new-profile', 'data-browse', 'preview', 'tgp-model', 'model-components',
+        'tree','popups','properties','pick','properties-menu','save','open-project','new-project', 'references', 'component-header',
+        'suggestions','undo','edit-source','jb-editor','jb-editor-styles','style-editor','probe','testers', 'event-tracker','h-to-jsx',
+      ],
+      'studio-tests': [
+        'probe','model','tree','suggestion'
+      ],
+      'css-files': [
+        'node_modules/material-design-lite/material.min.css',
+        'node_modules/material-design-lite/dist/material.indigo-pink.min.css',
+
+        'css/font.css',
+        'css/styles.css',
+      ],
+      'deep-diff': [
+        'dist/deep-diff.js',
+      ],
+      babel: [
+        'node_modules/babel-standalone/babel.js',
+        'dist/babel-ext.js'
+      ],
+      'node-adapter': [ 'src/node-adapter/node-adapter.js' ],
+      'pretty-print': [ 'src/misc/pretty-print.js' ],
+      'object-encoder': [ 'src/misc/object-encoder.js' ],
+      'xml': [ 'src/misc/xml.js' ],
+      'jison': [ 'dist/jb-jison.js', 'src/misc/jison.js' ],
+      'parsing': [ 'src/misc/parsing.js' ],
+      'spy': [ 'src/misc/spy.js' ],
+      'node': [ 'src/core/jb-node.js' ],
+});
+
+function jb_dynamicLoad(modules,prefix) {
+  prefix = prefix || '';
+  modules.split(',').forEach(m=>{
+    (resources[m] || []).forEach(file=>{
+      if (m == 'studio')
+        file = 'projects/studio/studio-' + file + '.js';
+      if (m == 'studio-tests')
+        file = 'projects/studio-helper/studio-' + file + '-tests.js';
+      // if (m=='node_modules/jquery/dist/jquery.min.js' && electron)
+      //   return document.write('<script src="../node_modules/jquery/dist/jquery.min.js" onload="global.$ = window.$ = window.jQuery = module.exports;"></script>');
+
+      if (prefix) { // avoid muliple source files with the same name in the debugger
+        const file_path = file.split('/');
+        file_path.push(prefix+file_path.pop());
+        file = file_path.join('/');
+      }
+      // if (win.electron)
+      //   return win.loadURL(`file://${win.jbartBase}/../${file}`)
+
+      const url = (window.jbLoaderRelativePath ? '' : '/') + file;
+      if (file.match(/\.js$/))
+        document.write('<script src="' + url + '" charset="UTF-8"></script>')
+      else
+        document.write('<link rel="stylesheet" type="text/css" href="' + url + '" />');
+    })
+  })
+}
+
+if (typeof window != 'undefined')
+  if (document.currentScript && document.currentScript.getAttribute('modules'))
+    jb_dynamicLoad(document.currentScript.getAttribute('modules'),document.currentScript.getAttribute('prefix'));
+
+if (typeof global != 'undefined')
+ global.resources = resources;
+;
+
---
+++ dist/testers.js
@@ -0,0 +1,239 @@
+jb.component('data-test', {
+	type: 'test',
+	params: [
+		{ id: 'calculate', dynamic: true },
+		{ id: 'runBefore', type: 'action', dynamic: true },
+		{ id: 'expectedResult', type: 'boolean', dynamic: true },
+		{ id: 'cleanUp', type: 'action', dynamic: true },
+		{ id: 'expectedCounters', as: 'single' }
+	],
+	impl: function(context,calculate,runBefore,expectedResult,cleanUp,expectedCounters) {
+		var initial_resources = jb.valueByRefHandler.resources && jb.valueByRefHandler.resources();
+		var initial_comps = jb.studio && jb.studio.compsRefHandler && jb.studio.compsRefHandler.resources();
+		if (expectedCounters) {
+			if (!jb.frame.wSpy.enabled())
+				jb.frame.initwSpy({wSpyParam: 'data-test'})
+			jb.frame.wSpy.clear()
+		}
+		return Promise.resolve(runBefore())
+			.then(_ =>
+				calculate())
+			.then(v=>
+				Array.isArray(v) ? jb.synchArray(v) : v)
+			.then(value=> {
+				const countersErr = countersErrors(expectedCounters);
+				const success = !! (expectedResult(new jb.jbCtx(context,{ data: value })) && !countersErr);
+				return { id: context.vars.testID, success, reason: countersErr}
+			})
+			.then(result => { // default cleanup
+				if (expectedCounters)
+					jb.frame.initwSpy({resetwSpyToNoop: true})
+				jb.valueByRefHandler.resources && jb.valueByRefHandler.resources(initial_resources);
+				jb.studio && jb.studio.compsRefHandler && jb.studio.compsRefHandler.resources(initial_comps);
+				return result;
+			}).then(result =>
+					Promise.resolve(cleanUp()).then(_=>result) )
+	}
+})
+
+jb.component('ui-test', {
+	type: 'test',
+	params: [
+		{ id: 'control', type: 'control', dynamic: true },
+		{ id: 'runBefore', type: 'action', dynamic: true },
+		{ id: 'action', type: 'action', dynamic: true },
+		{ id: 'expectedResult', type: 'boolean', dynamic: true },
+		{ id: 'cleanUp', type: 'action', dynamic: true },
+		{ id: 'expectedCounters', as: 'single' }
+	],
+	impl: function(context,control,runBefore,action,expectedResult,cleanUp,expectedCounters) {
+		var initial_resources = jb.valueByRefHandler.resources();
+		var initial_comps = jb.studio && jb.studio.compsRefHandler && jb.studio.compsRefHandler.resources();
+		return Promise.resolve(runBefore())
+			.then(_ => {
+				try {
+					if (expectedCounters) {
+						if (!jb.frame.wSpy.enabled())
+							jb.frame.initwSpy({wSpyParam: 'ui-test'})
+						jb.frame.wSpy.clear()
+					}
+					var elem = document.createElement('div');
+					var vdom = jb.ui.h(jb.ui.renderable(control()));
+					var cmp = jb.ui.render(vdom, elem)._component;
+					return Promise.resolve(cmp && cmp.delayed).then(_=>
+						elem)
+				} catch (e) {
+					jb.logException(e,'error in test',context);
+					return document.createElement('div');
+				}
+			})
+			.then(elem =>
+				Promise.resolve(action(context.setVars({elemToTest : elem }))).then(_=>elem))
+			.then(elem=> {
+				// put input values as text
+				Array.from(elem.querySelectorAll('input')).forEach(e=>{
+          if (e.parentNode)
+            jb.ui.addHTML(e.parentNode,`<input-val style="display:none">${e.value}</input-val>`)
+				})
+				const countersErr = countersErrors(expectedCounters);
+				const success = !! (expectedResult(new jb.jbCtx(context,{ data: elem.outerHTML })) && !countersErr);
+				return { id: context.vars.testID, success, elem, reason: countersErr}
+			}).then(result=> { // default cleanup
+				if (new URL(location.href).searchParams.get('show') === null) {
+					jb.ui.dialogs.dialogs.forEach(d=>d.close())
+					jb.valueByRefHandler.resources(initial_resources);
+					jb.studio && jb.studio.compsRefHandler && jb.studio.compsRefHandler.resources(initial_comps);
+					if (expectedCounters)
+						jb.frame.initwSpy({resetwSpyToNoop: true})
+				}
+				return result;
+			}).then(result =>
+				Promise.resolve(cleanUp()).then(_=>result) )
+	}
+})
+
+function countersErrors(expectedCounters) {
+	return Object.keys(expectedCounters || {}).map(
+		counter => expectedCounters[counter] != jb.frame.wSpy.logs.$counters[counter] 
+			? `${counter}: ${jb.frame.wSpy.logs.$counters[counter]} instead of ${expectedCounters[counter]}` : '')
+		.filter(x=>x)
+		.join(', ')
+}
+
+jb.component('ui-action.click', {
+	type: 'ui-action',
+	params: [
+		{ id: 'selector', as: 'string' },
+	],
+	impl: (ctx,selector,value) => {
+		var elems = selector ? Array.from(ctx.vars.elemToTest.querySelectorAll(selector)) : [ctx.vars.elemToTest];
+		elems.forEach(e=>
+			e._component && e._component.clicked && e._component.clicked())
+//			e.click())
+		return jb.delay(1);
+	}
+})
+
+jb.component('ui-action.keyboard-event', {
+	type: 'ui-action',
+	params: [
+		{ id: 'selector', as: 'string' },
+		{ id: 'type', as: 'string', options: ['keypress','keyup','keydown'] },
+		{ id: 'keyCode', as: 'number' },
+		{ id: 'ctrl', as: 'string', options: ['ctrl','alt'] },
+	],
+	impl: (ctx,selector,type,keyCode,ctrl) => {
+		var elems = selector ? Array.from(ctx.vars.elemToTest.querySelectorAll(selector)) : [ctx.vars.elemToTest];
+		elems.forEach(el=> {
+				var e = new KeyboardEvent(type,{
+					ctrlKey: ctrl == 'ctrl', altKey: ctrl == 'alt'
+				});
+				Object.defineProperty(e, 'keyCode', { get : _ => keyCode });
+				el.dispatchEvent(e);
+			})
+		return jb.delay(1);
+	}
+})
+
+jb.component('ui-action.set-text', {
+	type: 'ui-action',
+	params: [
+		{ id: 'value', as: 'string', essential: true },
+		{ id: 'selector', as: 'string', defaultValue: 'input' },
+		{ id: 'delay', as: 'number', defaultValue: 1}
+	],
+	impl: (ctx,value,selector,delay) => {
+		var elems = selector ? Array.from(ctx.vars.elemToTest.querySelectorAll(selector)) : [ctx.vars.elemToTest];
+		elems.forEach(e=> {
+			e._component.jbModel(value);
+			jb.ui.findIncludeSelf(e,'input').forEach(el=>el.value = value);
+		})
+		return jb.delay(delay);
+	}
+})
+
+jb.component('test.dialog-content', {
+	type: 'data',
+	params: [
+		{ id: 'id', as: 'string' },
+	],
+	impl: (ctx,id) =>
+		jb.ui.dialogs.dialogs.filter(d=>d.id == id).map(d=>d.el)[0].outerHTML || ''
+})
+
+var jb_success_counter = 0;
+var jb_fail_counter = 0;
+
+function goto_editor(id) {
+	fetch(`/?op=gotoSource&comp=${id}`)
+}
+function hide_success_lines() {
+	document.querySelectorAll('.success').forEach(e=>e.style.display = 'none')
+}
+
+function isCompNameOfType(name,type) {
+	const comp = name && jb.comps[name];
+	if (comp) {
+		while (jb.comps[name] && !jb.comps[name].type && jb.compName(jb.comps[name].impl))
+			name = jb.compName(jb.comps[name].impl);
+		return (jb.comps[name] && jb.comps[name].type || '').indexOf(type) == 0;
+	}
+}
+jb.ui = jb.ui || {}
+jb.ui.addHTML = jb.ui.addHTML || ((el,html) => {
+	var elem = document.createElement('div');
+	elem.innerHTML = html;
+	el.appendChild(elem.firstChild)
+})
+  
+
+startTime = startTime || new Date().getTime();
+jb.testers.runTests = function(testType,specificTest,show,rerun) {
+	var tests = jb.entries(jb.comps)
+		.filter(e=>typeof e[1].impl == 'object')
+		.filter(e=>e[1].type != 'test') // exclude the testers
+		.filter(e=>isCompNameOfType(e[0],'test'))
+		.filter(e=>!testType || e[1].impl.$ == testType)
+		.filter(e=>!specificTest || e[0] == specificTest);
+
+
+	document.write(`<div style="font-size: 20px"><span id="fail-counter" onclick="hide_success_lines()"></span><span id="success-counter"></span><span>, total ${tests.length}</span><span id="time"></span><span id="memory-usage"></span></div>`);
+
+	return jb.rx.Observable.from(Array.from(Array(rerun ? Number(rerun) : 1).keys()))
+		.concatMap(i=> (i % 20 == 0) ? jb.delay(300): [1])
+		.concatMap(_=>
+		jb.rx.Observable.from(tests).concatMap(e=>{
+			jb.logs.error = [];
+			return Promise.resolve(new jb.jbCtx().setVars({testID: e[0]}).run({$:e[0]}))
+				.then(res => {
+					if (res.success && jb.logs.error.length > 0) {
+						res.success = false;
+						res.reason = 'log errors: ' + JSON.stringify(jb.logs.error) 
+					}
+					return res
+				})
+			})).subscribe(res=> {
+				if (res.success)
+					jb_success_counter++;
+				else
+					jb_fail_counter++;
+				const baseUrl = window.location.href.split('/tests.html')[0]
+				var elem = `<div class="${res.success ? 'success' : 'failure'}""><a href="${baseUrl}/tests.html?test=${res.id}&show&wspy=res" style="color:${res.success ? 'green' : 'red'}">${res.id}</a>
+				<button class="editor" onclick="goto_editor('${res.id}')">src</button><span>${res.reason||''}</span>
+				</div>`;
+
+				document.getElementById('success-counter').innerHTML = ', success ' + jb_success_counter;
+				document.getElementById('fail-counter').innerHTML = 'failures ' + jb_fail_counter;
+				document.getElementById('fail-counter').style.color = jb_fail_counter ? 'red' : 'green';
+				document.getElementById('fail-counter').style.cursor = 'pointer';
+				document.getElementById('memory-usage').innerHTML = ', ' + (performance.memory.usedJSHeapSize / 1000000)  + 'M memory used';
+
+				document.getElementById('time').innerHTML = ', ' + (new Date().getTime() - startTime) +' mSec';
+				jb.ui.addHTML(document.body,elem);
+				if (show && res.elem)
+					document.body.appendChild(res.elem);
+				jb.ui && jb.ui.garbageCollectCtxDictionary && jb.ui.garbageCollectCtxDictionary(true)
+			})
+}
+;
+
