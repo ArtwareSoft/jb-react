6fa58821be6a40e801db9584f36e093764a5af13
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2019-09-05 19:56:45 +0300

    tgp-pretty

--- projects/studio-helper/studio-suggestion-tests.js
+++ projects/studio-helper/studio-suggestion-tests.js
@@ -5,42 +5,44 @@ jb.component('people-array', { watchableData: { "people": [
   ]
 }})
 
-jb.component('suggestions-test.default-probe', {
-	type: 'control',
-	impl :{$: 'label', title: ''}
+jb.component('suggestions-test.default-probe', { /* suggestionsTest.defaultProbe */
+  type: 'control',
+  impl: label(
+    ''
+  )
 })
 
-jb.component('suggestions-test.simple-vars', {
-	 impl :{$: 'suggestions-test', 
-	 	expression: '%',
-		expectedResult :{$: 'contains', text: '$people' }
-	},
+jb.component('suggestions-test.simple-vars', { /* suggestionsTest.simpleVars */
+  impl: suggestionsTest({
+    expression: '%',
+    expectedResult: contains('$people')
+  })
 })
 
-jb.component('suggestions-test.vars-filter', {
-	 impl :{$: 'suggestions-test', 
-	 	expression: '%$p',
-		expectedResult :{ $and: [{$: 'contains', text: '$people' }, { $not: { $contains: '$win'}}]}
-	},
+jb.component('suggestions-test.vars-filter', { /* suggestionsTest.varsFilter */
+  impl: suggestionsTest({
+    expression: '%$p',
+    expectedResult: and(contains('$people'), not(contains({})))
+  })
 })
 
-jb.component('suggestions-test.component', {
-	 impl :{$: 'suggestions-test', 
-	 	expression: '=pi',
-		expectedResult :{$: 'contains', text: 'pipeline' }
-	},
+jb.component('suggestions-test.component', { /* suggestionsTest.component */
+  impl: suggestionsTest({
+    expression: '=pi',
+    expectedResult: contains('pipeline')
+  })
 })
 
-jb.component('suggestions-test.inside-array', {
-	 impl :{$: 'suggestions-test', 
-	 	expression: '%$people-array/',
-		expectedResult :{ $and: [{$: 'contains', text: 'people' }, { $not: { $contains: '$people'}}]}
-	},
+jb.component('suggestions-test.inside-array', { /* suggestionsTest.insideArray */
+  impl: suggestionsTest({
+    expression: '%$people-array/',
+    expectedResult: and(contains('people'), not(contains({})))
+  })
 })
 
-jb.component('suggestions-test.1', {
-	 impl :{$: 'suggestions-test', 
-	 	expression: '%',
-		expectedResult :{$: 'contains', text: 'people' }
-	},
+jb.component('suggestions-test.1', { /* suggestionsTest.1 */ 
+  impl: suggestionsTest({
+    expression: '%',
+    expectedResult: contains('people')
+  })
 })
