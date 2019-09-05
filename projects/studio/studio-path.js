a41a978a9ae945327f8ecd3e56077cb1abf85db7
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2019-09-05 18:37:12 +0300

    array-in-resource-bug

--- projects/studio/studio-path.js
+++ projects/studio/studio-path.js
@@ -39,7 +39,7 @@ st.initCompsRefHandler = function(previewjb,allowedTypes) {
 function writeValueToDataResource(path,value) {
 	if (path.length > 1 && ['watchableData','passiveData'].indexOf(path[1]) != -1) {
 		const dataPath = '%$' + [path[0], ...path.slice(2)].map(x=>isNaN(+x) ? x : `[${x}]`).join('/') + '%'
-		return (new st.previewjb.jbCtx()).run(writeValue(dataPath,value))
+		return (new st.previewjb.jbCtx()).run(writeValue(dataPath,_=>value))
 	}
 }
 
