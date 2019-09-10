ed27509a53cd88a6e80a50e21c2b759cd5659500
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2019-09-10 22:13:15 +0300

    data-changes-are-saved

--- projects/hello-world/hello-world.js
+++ projects/hello-world/hello-world.js
@@ -1,8 +1,19 @@
-jb.component('people', { watchableData: [
-  { "name": "Homer Simpson" ,age: 42 , male: true, children: [{ name: 'Bart' }, { name: 'Lisa' }, { name: 'Maggie' } ]},
-  { "name": "Marge Simpson" ,age: 38 , male: false, children: [{ name: 'Bart' }, { name: 'Lisa' }, { name: 'Maggie' } ]},
-  { "name": "Bart Simpson"  ,age: 12 , male: true, children: []}
-]
+jb.component('people', { /* people */
+  watchableData: [
+    {
+      name: 'Homer Simpson',
+      age: 42,
+      male: false,
+      children: [{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}]
+    },
+    {
+      name: 'Marge Simpson',
+      age: 38,
+      male: true,
+      children: [{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}]
+    },
+    {name: 'Bart Simpson', age: 12, male: false, children: []}
+  ]
 })
 
 jb.component('hello-world.main', { /* helloWorld.main */
