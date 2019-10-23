3be87c7f642a1b7c66f1e56f380a7319b146d401
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2019-10-23 15:30:25 +0300

    fixes

--- dist/jb4node.js
+++ dist/jb4node.js
@@ -2235,7 +2235,6 @@ if (typeof window == 'object') {
 })()
 ;
 
-
 jb.component('pretty-print', { /* prettyPrint */
   params: [
     {id: 'profile', defaultValue: '%%'},
@@ -2262,7 +2261,7 @@ jb.prettyPrint.advanceLineCol = function({line,col},text) {
   const newCol = noOfLines ? text.match(/\n(.*)$/)[1].length : col + text.length
   return { line: line + noOfLines, col: newCol }
 }
-jb.prettyPrint.spaces = Array.from(new Array(200)).map(_=>' ').join('')
+jb.prettyPrint.spaces = Array.from(new Array(200)).map(_=>' ').join('');
 
 jb.prettyPrintWithPositions = function(val,{colWidth=80,tabSize=2,initialPath='',showNulls,comps} = {}) {
   comps = comps || jb.comps
@@ -2407,7 +2406,7 @@ jb.prettyPrintWithPositions = function(val,{colWidth=80,tabSize=2,initialPath=''
       else
         return JSON.stringify(val); // primitives
     }
-}
+  }
 
   function arrayToMacro({path, line, col}, array, flat) {
     const ctx = {path, line, col}
@@ -2419,154 +2418,7 @@ jb.prettyPrintWithPositions = function(val,{colWidth=80,tabSize=2,initialPath=''
   }
 }
 
-//   function rawFormat() {
-//     let remainedInLine = colWidth;
-//     let result = '';
-//     let depth = 0;
-//     let lineNum = 0;
-//     let positions = {};
-//     printValue(profile,initialPath || '');
-//     return { result, positions }
-
-//     function sortedPropertyNames(obj) {
-//       let props = jb.entries(obj)
-//         .filter(p=>showNulls || p[1] != null)
-//         .map(x=>x[0]) // try to keep the order
-//         .filter(p=>p.indexOf('$jb') != 0)
-
-//       const comp_name = jb.compName(obj);
-//       if (comp_name) { // tgp obj - sort by params def
-//         const params = jb.compParams(comps[comp_name]).map(p=>p.id);
-//         props.sort((p1,p2)=>params.indexOf(p1) - params.indexOf(p2));
-//       }
-//       if (props.indexOf('$') > 0) { // make the $ first
-//         props.splice(props.indexOf('$'),1);
-//         props.unshift('$');
-//       }
-//       return props;
-//     }
-
-//     function printValue(val,path) {
-//       positions[path] = lineNum;
-//       if (!val) return;
-//       if (val.$jb_arrayShortcut)
-//         val = val.items;
-//       if (Array.isArray(val)) return printArray(val,path);
-//       if (typeof val === 'object') return printObj(val,path);
-//       if (typeof val === 'function')
-//         result += val.toString();
-//       else if (typeof val === 'string' && val.indexOf("'") == -1 && val.indexOf('\n') == -1)
-//         result += "'" + JSON.stringify(val).replace(/^"/,'').replace(/"$/,'') + "'";
-//       else if (typeof val === 'string' && val.indexOf('\n') != -1) {
-//         result += "`" + val.replace(/`/g,'\\`') + "`"
-//       } else {
-//         result += JSON.stringify(val);
-//       }
-//     }
-
-//     function printObj(obj,path) {
-//         var obj_str = flat_obj(obj);
-//         if (!printInLine(obj_str)) { // object does not fit in parent line
-//           depth++;
-//           result += '{';
-//           if (!printInLine(obj_str)) { // object does not fit in its own line
-//             sortedPropertyNames(obj).forEach(function(prop,index,array) {
-//                 if (prop != '$')
-//                   newLine();
-//                 if (showNulls || obj[prop] != null) {
-//                   printProp(obj,prop,path);
-//                   if (index < array.length -1)
-//                     result += ', ';//newLine();
-//                 }
-//             });
-//           }
-//           depth--;
-//           newLine();
-//           result += '}';
-//         }
-//     }
-//     function quotePropName(p) {
-//       if (p.match(/^[$a-zA-Z_][$a-zA-Z0-9_]*$/))
-//         return p;
-//       else
-//         return `"${p}"`
-//     }
-//     function printProp(obj,prop,path) {
-//       if (obj[prop] && obj[prop].$jb_arrayShortcut)
-//         obj = obj(prop,obj[prop].items);
-
-//       if (printInLine(flat_property(obj,prop))) return;
-
-//       if (prop == '$')
-//         result += '$: '
-//       else
-//         result += quotePropName(prop) + (jb.compName(obj[prop]) ? ' :' : ': ');
-//       //depth++;
-//       printValue(obj[prop],path+'~'+prop);
-//       //depth--;
-//     }
-//     function printArray(array,path) {
-//       if (printInLine(flat_array(array))) return;
-//       result += '[';
-//       depth++;
-//       newLine();
-//       array.forEach(function(val,index) {
-//         printValue(val,path+'~'+index);
-//         if (index < array.length -1) {
-//           result += ', ';
-//           newLine();
-//         }
-//       })
-//       depth--;newLine();
-//       result += ']';
-//     }
-//     function printInLine(text) {
-//       if (remainedInLine < text.length || text.match(/:\s?{/) || text.match(/, {\$/)) return false;
-//       result += text;
-//       remainedInLine -= text.length;
-//       return true;
-//     }
-//     function newLine() {
-//       result += '\n';
-//       lineNum++;
-//       for (var i = 0; i < depth; i++) result += '               '.substr(0,tabSize);
-//       remainedInLine = colWidth - tabSize * depth;
-//     }
-
-//     function flat_obj(obj) {
-//       var props = sortedPropertyNames(obj)
-//         .filter(p=>showNulls || obj[p] != null)
-//         .filter(x=>x!='$')
-//         .map(prop =>
-//         quotePropName(prop) + ': ' + flat_val(obj[prop]));
-//       if (obj && obj.$) {
-//         props.unshift("$: '" + obj.$+ "'");
-//         return '{' + props.join(', ') + ' }'
-//       }
-//       return '{ ' + props.join(', ') + ' }'
-//     }
-//     function flat_property(obj,prop) {
-//       if (jb.compName(obj[prop]))
-//         return quotePropName(prop) + ' :' + flat_val(obj[prop]);
-//       else
-//         return quotePropName(prop) + ': ' + flat_val(obj[prop]);
-//     }
-//     function flat_val(val) {
-//       if (Array.isArray(val)) return flat_array(val);
-//       if (typeof val === 'object') return flat_obj(val);
-//       if (typeof val === 'function') return val.toString();
-//       if (typeof val === 'string' && val.indexOf("'") == -1 && val.indexOf('\n') == -1)
-//         return "'" + JSON.stringify(val).replace(/^"/,'').replace(/"$/,'') + "'";
-//       else
-//         return JSON.stringify(val); // primitives
-//     }
-//     function flat_array(array) {
-//       return '[' + array.map(item=>flat_val(item)).join(', ') + ']';
-//     }
-
-//   }
-
-// };
+;
 
 jb.xml = jb.xml || {}
 
