319cef357eac3712545474732290ca0b9253c57c
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2020-02-06 08:32:29 +0200

    worker

---
+++ projects/ui-tests/remote-widgets.js
@@ -0,0 +1,54 @@
+jb.component('watchable-people', { /* watchablePeople */
+    watchableData: [
+        {name: 'Homer Simpson - watchable', age: 42, male: true},
+        {name: 'Marge Simpson - watchable', age: 38, male: false},
+        {name: 'Bart Simpson - watchable', age: 12, male: true}
+    ]
+})
+
+jb.component('people', { /* people */
+    passiveData: [
+        {name: 'Homer Simpson', age: 42, male: true},
+        {name: 'Marge Simpson', age: 38, male: false},
+        {name: 'Bart Simpson', age: 12, male: true}
+    ]
+})
+
+jb.component('person', { /* person */
+    watchableData: {
+        name: 'Homer Simpson',
+        male: true,
+        isMale: 'yes',
+        age: 42
+    }
+})
+
+jb.component('personWithAddress', { /* personWithAddress */
+    watchableData: {
+        name: 'Homer Simpson',
+        address: {city: 'Springfield', street: '742 Evergreen Terrace'}
+    }
+})
+
+jb.component('personWithChildren', { /* personWithChildren */
+    watchableData: {
+        name: 'Homer Simpson',
+        children: [{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}],
+        friends: [{name: 'Barnie'}]
+    }
+})
+
+jb.component('ui-test.remote-editable-ctrl', {
+    type: 'control',
+    impl: group({
+        controls: [
+          editableText({title: 'name', databind: '%$person/name%', features: id('inp') }),
+          text('%$person/name%')
+        ]
+    })
+})
+
+jb.component('ui-test.hello-from-worker', {
+    type: 'control',
+    impl: text('hello from worker')
+})
---
+++ src/ui/core/vdom.js
@@ -0,0 +1,70 @@
+class VNode {
+    constructor(cmpOrTag, _attributes, _children) {
+        const attributes = jb.objFromEntries(jb.entries(_attributes).map(e=>[e[0].toLowerCase(),e[1]]))
+        let children = (_children === '') ? null : _children
+        if (['string','boolean','number'].indexOf(typeof children) !== -1) {
+            attributes.$text = children
+            children = null
+        }
+        if (children && typeof children.then == 'function') {
+            attributes.$text = '...'
+            children = null
+        }
+        if (children != null && !Array.isArray(children)) children = [children]
+        if (children != null)
+            children = children.filter(x=>x).map(item=> typeof item == 'string' ? h('span',{$text: item}) : item)
+        Object.assign(this,{...{[typeof cmpOrTag === 'string' ? 'tag' : 'cmp'] : cmpOrTag} ,attributes,children})
+    }
+    getAttribute(att) {
+        return (this.attributes || {})[att]
+    }
+    setAttribute(att,val) {
+        this.attributes = this.attributes || {}
+        this.attributes[att] = val
+    }
+    addClass(clz) {
+        this.attributes = this.attributes || {};
+        if (this.attributes.class === undefined) this.attributes.class = ''
+        if (clz && this.attributes.class.split(' ').indexOf(clz) == -1)
+            this.attributes.class = [this.attributes.class,clz].filter(x=>x).join(' ');
+        return this;
+    }
+    hasClass(clz) {
+        return (jb.path(this,'attributes.class') || '').split(' ').indexOf(clz) != -1
+    }
+    querySelectorAll(selector,{includeSelf}={}) {
+        const hasAtt = selector.match(/^\[([a-zA-Z0-9_\-]+)\]$/)
+        const attEquals = selector.match(/^\[([a-zA-Z0-9_\-]+)="([a-zA-Z0-9_\-]+)"\]$/)
+        const hasClass = selector.match(/^\.([a-zA-Z0-9_\-]+)$/)
+        const hasTag = selector.match(/^[a-zA-Z0-9_\-]+$/)
+        const selectorMatcher = hasAtt ? el => el.attributes && el.attributes[hasAtt[1]]
+            : hasClass ? el => el.hasClass(hasClass[1])
+            : hasTag ? el => el.tag === hasTag[0]
+            : attEquals ? el => el.attributes && el.attributes[attEquals[1]] == attEquals[2]
+            : null
+
+        return selectorMatcher && doFind(this,selectorMatcher,!includeSelf)
+
+        function doFind(vdom,selectorMatcher,excludeSelf) {
+            return [ ...(!excludeSelf && selectorMatcher(vdom) ? [vdom] : []), 
+                ...(vdom.children||[]).flatMap(ch=> doFind(ch,selectorMatcher))
+            ]
+        }
+    }
+}
+
+function toVdomOrStr(val) {
+    if (val &&  (typeof val.then == 'function' || typeof val.subscribe == 'function'))
+        return jb.synchArray(val).then(v => jb.ui.toVdomOrStr(v[0]))
+
+    const res1 = Array.isArray(val) ? val.map(v=>jb.val(v)): val
+    let res = jb.val((Array.isArray(res1) && res1.length == 1) ? res1[0] : res1)
+    if (res && res instanceof VNode || Array.isArray(res)) return res
+    if (typeof res === 'boolean' || typeof res === 'object')
+        res = '' + res
+    else if (typeof res === 'string')
+        res = res.slice(0,1000)
+    return res
+}
+
+Object.assign(jb.ui, {VNode, toVdomOrStr})
