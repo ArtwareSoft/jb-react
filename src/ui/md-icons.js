1c01aca561a0aa8424b470074adda90724007e31
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2020-02-21 15:40:30 +0200

    mdi-icons

---
+++ src/ui/md-icons.js
@@ -0,0 +1,12 @@
+jb.component('button.md-icon', { /* button.mdIcon */
+    type: 'button.style,icon-with-action.style',
+    params: [
+      {id: 'icon', as: 'string', defaultValue: 'Yoga'},
+      {id: 'raisedIcon', as: 'string'}
+    ],
+    impl: customStyle({
+      template: (cmp,{title,icon,raised,raisedIcon},h) => 
+          h('div',{$html: `<svg height="24" width="24"><path d="${jb.path(jb.frame,['MDIcons',icon])}"/></svg>`}),
+      css: '{width: 24px; height: 24px}'
+    })
+})
