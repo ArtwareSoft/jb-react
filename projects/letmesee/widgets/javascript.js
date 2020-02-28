ef779021384dc462974f4467a20b3f516798a0dd
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2020-02-28 08:28:10 +0200

    build-lib

---
+++ projects/letmesee/widgets/javascript.js
@@ -0,0 +1,29 @@
+aa_lmcApi_registerPlugin({
+  id: 'javascript',
+  title: 'javascript',
+  editor: {
+    js(object,settingsRef) {
+      var editor_elem = object.el
+      if (!editor_elem) return
+      editor_elem.value = settingsRef()
+      editor_elem.addEventListener("blur", () => settingsRef(editor_elem.value))
+    },
+    html: '<textarea id="scriptEditor" rows="10" cols="80"/>',
+    css: '#this { }',
+    defaultWidgetData: 'enter your script here',
+    files: []
+},
+  runtime: {
+    js(object,data) {
+      if (!data || typeof data != 'string') return
+      try {
+        eval(data)
+      } catch(e) {}
+    },
+    html: '',
+    css: '',
+    files: []
+},   
+})
+
+aa_lmcWidget_javascript = true
