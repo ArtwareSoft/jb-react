64cc340cdeb32d770216d05b25d1305bd7faee34
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2020-04-21 22:08:09 +0300

    build-lib

--- bin/studio/studio-all.js
+++ bin/studio/studio-all.js
@@ -36257,7 +36257,7 @@ jb.component('studio.previewWidgetImpl', {
 
 st.injectProjectToPreview = function(previewWin,projectSettings) {
 const baseProjUrl = jb.frame.jbBaseProjUrl ? `jbBaseProjUrl = '${jbBaseProjUrl}'` : ''
-const jbModuleUrl = jb.frame.jbModuleUrl ? `jbModuleUrl = '${jbModuleUrl}'` : ''
+const moduleUrl = jb.frame.jbModuleUrl ? `jbModuleUrl = '${jbModuleUrl}'` : ''
 
 const vscodeZoomFix = jb.frame.jbInvscode? 'style="zoom: 0.8"' : ''
 const html = `<!DOCTYPE html>
@@ -36266,7 +36266,7 @@ const html = `<!DOCTYPE html>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script type="text/javascript">
     ${baseProjUrl}
-    ${jbModuleUrl}
+    ${moduleUrl}
     jbProjectSettings = ${JSON.stringify(projectSettings)}
   </script>
   <script type="text/javascript" src="${st.host.jbLoader}"></script>
