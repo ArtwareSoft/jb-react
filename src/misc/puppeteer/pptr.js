8d1f89e3bcb3a43c40a4f765d8f90936569f8f81
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2020-05-05 09:22:59 +0300

    build-lib

--- src/misc/puppeteer/pptr.js
+++ src/misc/puppeteer/pptr.js
@@ -23,11 +23,13 @@ jb.component('pptr.gotoPage', {
     rx.var('url', ({},{},{url}) => url),
     pptr.logActivity('start navigation', '%$url%'),
     rx.doPromise(({},{page},{url}) => page.goto(url)),
+    pptr.logActivity('after goto page', '%$url%'),
     rx.mapPromise((ctx,{},{frame}) => frame(ctx)),
     rx.var('frame'),
-    rx.doPromise(
-        ({},{frame},{waitUntil,timeout}) => frame.waitForNavigation({waitUntil, timeout})
-      ),
+    pptr.logActivity('frame', '%$frame%'),
+    // rx.doPromise(
+    //     ({},{frame},{waitUntil,timeout}) => frame.waitForNavigation({waitUntil, timeout})
+    //   ),
     pptr.logActivity('end navigation', '%$url%')
   )
 })
