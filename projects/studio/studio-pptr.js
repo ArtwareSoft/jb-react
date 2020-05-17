1f708776361b31f8728fa38093b3d9c9b4a82280
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2020-05-17 18:21:08 +0300

    build-lib

---
+++ projects/studio/studio-pptr.js
@@ -0,0 +1,23 @@
+jb.component('studio.pptrToolbar', {
+  type: 'control',
+  params: [
+    {id: 'path', as: 'string'},
+    {id: 'expanded', as: 'boolean', type: 'boolean'}
+  ],
+  impl: group({
+    controls: [
+      controlWithCondition('%val.$%==pptr.session%', text('run icon')),
+      controlWithCondition(
+        studio.isOfType('%$path%', 'pptr'),
+        text('runninng indicator')
+      ),
+      text('')
+    ],
+    features: [
+      group.firstSucceeding(),
+      studio.watchPath({path: '%$path%', includeChildren: 'yes', recalcVars: true}),
+      variable({name: 'paramDef', value: studio.paramDef('%$path%')}),
+      variable({name: 'val', value: studio.val('%$path%')})
+    ]
+  })
+})
