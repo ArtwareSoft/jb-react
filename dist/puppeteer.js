08de5ce1055962066706cc2f3a19cb7665a85554
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2020-05-22 18:55:21 +0300

    build-lib

--- dist/puppeteer.js
+++ dist/puppeteer.js
@@ -239,7 +239,7 @@ jb.component('pptr.getProperty', {
     params: [
       {id: 'propName', as: 'string',  options: 'value,innerHTML,outerHTML,href,textContent', mandatory: true}
     ],
-    impl: rx.mapPromise((ctx,{},{propName}) => jb.pptr.runMethod(ctx,'evaluate',x=>x[propName])),
+    impl: rx.mapPromise((ctx,{},{propName}) => jb.pptr.runMethod(ctx,'evaluate',(x,prop)=>x[prop],ctx.data,propName )),
 })
 
 jb.component('pptr.eval', {
