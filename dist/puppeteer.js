8fb9f1029371c509b2fb8b8706b62c77bb4d26cb
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2020-05-25 21:44:25 +0300

    build-lib

--- dist/puppeteer.js
+++ dist/puppeteer.js
@@ -220,7 +220,7 @@ jb.component('pptr.jsFunction', {
     impl: rx.innerPipe(
         rx.mapPromise((ctx,{},{expression}) => jb.pptr.runMethod(ctx,'evaluate',expression)),
         rx.catchError(ctx=>null),
-        rx.do((ctx,{},{expression}) => (ctx.data == null) && pptrSession.events.next({$: 'Activity', activity: 'catch', description: `${expression} failed`, ctx }) )
+        rx.do((ctx,{pptrSession},{expression}) => (ctx.data == null) && pptrSession.events.next({$: 'Activity', activity: 'catch', description: `${expression} failed`, ctx }) )
     )
 })
 
