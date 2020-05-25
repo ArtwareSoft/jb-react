3d4caa61170321d92d937a374e102468ed5e49d4
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2020-05-25 21:50:04 +0300

    build-lib

--- dist/puppeteer.js
+++ dist/puppeteer.js
@@ -192,7 +192,7 @@ jb.component('pptr.selectElement', {
         {id: 'resultVar', as: 'string', description: 'empty for no var' },
 //        {id: 'onlyWait', as: 'boolean', description: 'returns the existing current value' },
     ],
-    impl: rx.retry({ operator: rx.innerPipe('%$startAt()%','%$select%', rx.var('%$resultVar%') ), interval: '%$retryInterval%', times: '%$retryTimes%'  }), 
+    impl: rx.innerPipe(rx.retry({ operator: '%$select%', interval: '%$retryInterval%', times: '%$retryTimes%'  }),rx.var('%$resultVar%')), 
 })
 // rx.map(ctx => ctx.run('%$startAt%')),
 
