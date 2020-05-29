60a937f0a713f7c1f95af9aca8ea6e3cfe284cba
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2020-05-29 09:53:27 +0300

    build-lib

---
+++ projects/puppeteerDemo/phones.js
@@ -0,0 +1,7 @@
+{"type":"error","desc":"Can not read file ./projects/puppeteerDemo/phones.js Error: ENOENT: no such file or directory, open 'C:\\jb-react\\projects\\puppeteerDemo\\phones.js'"}
+jb.component('dataResource.phones', {
+  watchableData: [
+    {title: 'aa', description: '', image: ''}
+  ],
+  type: ''
+})
