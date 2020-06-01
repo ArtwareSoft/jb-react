dd2efd13721be4760b0e5c4bbef6243fe3202e47
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2020-06-01 22:48:37 +0300

    build-lib

--- dist/remote.js
+++ dist/remote.js
@@ -5,6 +5,8 @@ jb.remote = {
         jb.callbag.map(m=> jb.remote.evalFunctions(JSON.parse(m.data))), 
         jb.callbag.filter(m=> m.id == id),
         jb.callbag.takeWhile(m=> !m.finished),
+        jb.callbag.Do(m => m.$ == 'cbLogByPathDiffs' && jb.remote.updateCbLogs(m.diffs) ),
+        jb.callbag.filter(m=> m.data),
         jb.callbag.map(m=> new jb.jbCtx().ctx({data: m.data.data, vars: m.data.vars, profile: '', forcePath: ''}))
     ),
     remoteSink: (remote, id) => source => jb.callbag.pipe(
@@ -37,19 +39,34 @@ jb.remote = {
         return obj
     },
     startCommandListener() {
-        const {pipe,filter,fromEvent,map,subscribe} = jb.callbag
-
+        const {pipe,interval,filter,fromEvent,map,subscribe} = jb.callbag
         pipe(
             fromEvent('message', self), 
             map(m=> jb.remote.evalFunctions(JSON.parse(m.data))),
             filter(m=> !m.id),
-            subscribe(m=> pipe(
+            subscribe(m=> {
+                pipe(
                     m.$ == 'innerCB' && jb.remote.remoteSource(self, m.sourceId),
                     new jb.jbCtx().ctx(m.ctx).runInner(m.profile, {type: 'rx'} ,m.propName),
                     jb.remote.remoteSink(self, m.sinkId),
+                    Do(e=> postMessage(JSON.stringify({$: 'cbLogByPathDiffs', id: m.sinkId, diffs: jb.remote.cbLogByPathDiffs(m.ctx.path)}))),
                     subscribe({complete: () => postMessage(JSON.stringify({id: m.sinkId, finished: true}))})
-            ))
+                )
+            })
         )
+    },
+    cbLogByPathDiffs(path) {
+        const entries = jb.entries(jb.cbLogByPath||{}).filter(e=>e[0].indexOf(path) == 0)
+        const res = jb.objFromEntries(entries.map(e=>[e[0],e[1].slice(e[1].lastDiff||0)]))
+        entries.forEach(e=>e[1].lastDiff = e[1].length)
+        return res
+    },
+    updateCbLogs(diffs) {
+        jb.cbLogByPath = jb.cbLogByPath || {}
+        jb.entries(diffs||{}).forEach(e=>{
+            jb.cbLogByPath[e[0]] = jb.cbLogByPath[e[0]] || { callbagLog: true, result: [] }
+            jb.cbLogByPath[e[0]].result = jb.cbLogByPath[e[0]].result.concat(e[1])
+        })
     }
 }
 
@@ -82,13 +99,13 @@ jb.component('remote.innerRx', {
         const sourceId = jb.remote.counter++
         const sinkId = jb.remote.counter++
         remote.postObj({ $: 'innerCB', sourceId, sinkId, propName: 'rx', profile: ctx.profile.rx, ctx })
+        jb.entries(jb.cbLogByPath||{}).filter(e=>e[0].indexOf(ctx.path) == 0).forEach(e=>e[1].result = [])
         return source => (start,sink) => {
             if (start!=0) return
             jb.delay(10).then(()=> jb.callbag.subscribe(()=>{})(jb.remote.remoteSink(remote,sourceId)(source)))
             const remoteSource = jb.remote.remoteSource(remote,sinkId)
             remoteSource(0, (t,d) => sink(t,d))
         }
-//        pipe(source,jb.remote.remoteSink(remote,sourceId), block, () => jb.remote.remoteSource(remote,sinkId))
     }
 })
 
@@ -100,6 +117,7 @@ jb.component('remote.sourceRx', {
     ],
     impl: (ctx,rx,remote) => {
         const sinkId = jb.remote.counter++
+        jb.entries(jb.cbLogByPath||{}).filter(e=>e[0].indexOf(ctx.path) == 0).forEach(e=>e[1].result = [])
         jb.delay(1).then(()=> remote.postObj({ $: 'sourceCB', sinkId, propName: 'rx', profile: ctx.profile.rx, ctx }))
         return jb.remote.remoteSource(remote,sinkId)
     }
