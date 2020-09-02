e44ee4076e9419a7ca79fff4ed7cdc399fb5342c
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2020-09-02 15:28:00 +0300

    build-lib

--- dist/jb-react-all.js
+++ dist/jb-react-all.js
@@ -7980,7 +7980,7 @@ jb.component('itemlist.infiniteScroll', {
       Var('shown','%$$state/visualLimit/shownItems%'),
       Var('itemsToAppend', pipeline('%$$props/allItems%',slice('%$shown%',math.plus('%$shown%','%$pageSize%')))),
       Var('delta', itemlist.deltaOfItems('%$itemsToAppend%', 
-        ({},{$state, shown, itemsToAppend}) => ({visualLimit: { ...$state.visualLimit, shownItems: shown + itemsToAppend} }))),
+        ({},{$state, shown, itemsToAppend}) => ({visualLimit: { ...$state.visualLimit, shownItems: shown + itemsToAppend.length} }))),
       //Var('cmpId','%$cmp/cmpId%'),
       action.applyDeltaToCmp('%$delta%','%$cmp/cmpId%')
     )),
