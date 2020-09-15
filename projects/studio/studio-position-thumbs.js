a187b8c5278cf59ea0dbc6ea9b657f5e31570e68
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2020-09-15 21:55:08 +0300

    build-lib

--- projects/studio/studio-position-thumbs.js
+++ projects/studio/studio-position-thumbs.js
@@ -20,17 +20,19 @@ Object.assign(jb.ui,{
   },
   studioFixYPos(elem) {
     if (elem && elem.ownerDocument == jb.frame.document) return 0
+    const doc = elem.ownerDocument
     //if (this._studioFixYPos == null) {
-      const zoom = +document.body.style.zoom || 1
-      this._studioFixYPos = ((document.querySelector('#jb-preview') && document.querySelector('#jb-preview').getBoundingClientRect().top) || 0)/zoom
+      const zoom = +doc.body.style.zoom || 1
+      
+      this._studioFixYPos = ((doc.querySelector('#jb-preview') && doc.querySelector('#jb-preview').getBoundingClientRect().top) || 0)/zoom
     //}
     return this._studioFixYPos
   },
   studioFixXPos(elem) {
     if (elem && elem.ownerDocument == jb.frame.document) return 0
+    const doc = elem.ownerDocument
     //if (this._studioFixXPos == null) {
-      const zoom = +document.body.style.zoom || 1
-      this._studioFixXPos = (document.querySelector('#jb-preview') && document.querySelector('#jb-preview').getBoundingClientRect().left) || 0
+      this._studioFixXPos = (doc.querySelector('#jb-preview') && doc.querySelector('#jb-preview').getBoundingClientRect().left) || 0
     //}
     return this._studioFixXPos
   }
