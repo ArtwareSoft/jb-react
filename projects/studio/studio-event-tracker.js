27f849d25bae6edc03d56cfe0c07c152f5977af5
Shai Ben-Yehuda <shaiby@artwaresoft.com>
2020-09-22 13:47:57 +0300

    build-lib

--- projects/studio/studio-event-tracker.js
+++ projects/studio/studio-event-tracker.js
@@ -135,7 +135,9 @@ jb.component('studio.eventTracker', {
             inGroup(list('exception','error'), '%logNames%'),
             css.color('var(--jb-error-fg)')
           )}),
-          controlWithCondition('%m%',text('%m/data/$%')), //%m/data/t%, %m/data/cbId%')),
+          group({controls:
+            controlWithCondition('%m%',text('%m/data/$%')), //%m/data/t%, %m/data/cbId%')),
+          }),
           studio.eventView()
         ],
         style: table.plain(true),
